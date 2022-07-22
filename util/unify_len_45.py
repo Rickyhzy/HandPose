0e6af93c4375ca8bd85b1a6cbd2c1c28f43c719b
Rickyhzy <674944243@qq.com>
2022-07-22 21:33:23 +0800

    latest

--- util/unify_len_45.py
+++ util/unify_len_45.py
@@ -12,14 +12,14 @@ def up_sample(data):
     x = np.arange(0, length)
 
     # 采用一维插值进行上采样
-    fc = interp1d(x, data, kind='cubic')
-    fc2 = interp1d(x, data, kind='nearest')
-    fc3 = interp1d(x, data, kind='linear')
+    fc = interp1d(x, data, kind='cubic', axis=0)
+    # fc2 = interp1d(x, data, kind='nearest')
+    # fc3 = interp1d(x, data, kind='linear')
 
     xint = np.linspace(x.min(), x.max(), 45)
     yint = fc(xint)
-    yint2 = fc2(xint)
-    yint3 = fc3(xint)
+    # yint2 = fc2(xint)
+    # yint3 = fc3(xint)
 
     # plt.figure(dpi=100)
     # plt.plot(yint, color='red', label='cubic')
@@ -47,21 +47,21 @@ def up_sample(data):
 def down_sample(data):
     length = len(data)
     x = np.arange(0, length)
-    print(x)
+    # print(x)
 
     # 利用插值法下采样
-    fc = interp1d(x, data, kind='cubic')
-    fc2 = interp1d(x, data, kind='nearest')
-    fc3 = interp1d(x, data, kind='linear')
+    fc = interp1d(x, data, kind='cubic', axis=0)
+    # fc2 = interp1d(x, data, kind='nearest')
+    # fc3 = interp1d(x, data, kind='linear')
 
     # 控制为45个采样点
     xint = np.linspace(x.min(), x.max(), 45)
-    print(xint)
+    # print(xint)
 
     # 进行插值downsample
     yint = fc(xint)
-    yint2 = fc2(xint)
-    yint3 = fc3(xint)
+    # yint2 = fc2(xint)
+    # yint3 = fc3(xint)
     # print(yint)
 
     # plt.figure(dpi=100)
@@ -84,11 +84,11 @@ def down_sample(data):
 
 
 if __name__ == '__main__':
-    one = pd.read_csv('../data/dynamic/data/a_sample_0_0.txt', header=None, delim_whitespace=True)
-    kkk = pd.read_csv('../data/dynamic/data/k_sample_0_46.txt', header=None, delim_whitespace=True)
-    print(len(one), len(kkk))
-    flex1_one = one.iloc[:, 0]
-    flex1_k = kkk.iloc[:, 0]
+    # one = pd.read_csv('../data/dynamic/data/a_sample_0_0.txt', header=None, delim_whitespace=True)
+    # kkk = pd.read_csv('../data/dynamic/data/k_sample_0_46.txt', header=None, delim_whitespace=True)
+    # print(len(one), len(kkk))
+    # flex1_one = one.iloc[:, 0]
+    # flex1_k = kkk.iloc[:, 0]
     # flex2 = one.iloc[:, 1]
     # flex3 = one.iloc[:, 2]
     # flex4 = one.iloc[:, 3]
@@ -110,11 +110,15 @@ if __name__ == '__main__':
         print(data)
         sam_len = len(data)
         if sam_len >= 45:
-            data_unify = down_sample(data)
+            data_unify = pd.DataFrame(down_sample(data))
         elif sam_len < 45:
-            data_unify = up_sample(data)
+            data_unify = pd.DataFrame(up_sample(data))
         else:
             data_unify = data
 
+        if not os.path.exists(target_path):
+            os.mkdir(target_path)
+        else:
+            print('文件夹已经存在')
         data_unify.to_csv(target_path + i, header=None, index_label=None, index=None)
 
