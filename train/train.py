a663f1aa84dfecf01bcd713e073348d44c5b7a6d
Rickyhzy <674944243@qq.com>
2022-09-22 00:05:53 +0800

    9.22 train code

--- train/train.py
+++ train/train.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 import os
 import glob
+import tqdm
 from cnn_dataset_one import CnnDataset
 from torch.utils.data import DataLoader
 
@@ -15,6 +16,7 @@ print("Device being used:", device)
 
 nEpochs = 100
 resume_epoch = 0
+nTestInterval = 20
 useTest = False
 snapshot = 50
 lr = 1e-3
@@ -44,7 +46,7 @@ modelName = 'elas1D'
 saveName = modelName + '-' + dataset
 
 def train_model(dataset=dataset, save_dir=save_dir, num_classes=num_classes, lr=lr,
-                num_epochs=nEpochs, save_epoch=snapshot, useTest=useTest):
+                num_epochs=nEpochs, save_epoch=snapshot, useTest=useTest, test_interval=nTestInterval):
 
     if modelName == 'elas1D':
         model = CNN_1D.CNN1D(num_classes=num_classes, pretrained=True)
@@ -103,16 +105,72 @@ def train_model(dataset=dataset, save_dir=save_dir, num_classes=num_classes, lr=
             for i, (inputs, labels) in enumerate(trainval_loaders[phase]):
                 inputs, labels = inputs.to(device), labels.to(device)
                 # print(i, inputs.shape,labels.shape)
+                # 梯度清零
+                optimizer.zero_grad()
 
-                # 前向计算，计算loss
-                out = model(inputs)
+
+                if phase == 'train':
+                    # 前向计算，计算loss
+                    out = model(inputs)
+                else:
+                    with torch.no_grad():
+                        out = model(inputs)
+
+                probs = torch.nn.Softmax(dim=1)(out)
+                preds = torch.max(probs, 1)[1]
                 loss = criterion(out, labels)
 
-                #梯度清零和反传
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
+                #梯度下降
+                if phase == 'train':
+                    loss.backward()
+                    optimizer.step()
+
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / trainval_sizes[phase]
+            epoch_acc = running_corrects.double() / trainval_sizes[phase]
+
+            print("[{}] Epoch: {}/{} Loss: {} Acc: {}".format(phase, epoch + 1, nEpochs, epoch_loss, epoch_acc))
+            stop_time = timeit.default_timer()
+            print("Execution time: " + str(stop_time - start_time) + "\n")
+
+            if epoch % save_epoch == (save_epoch - 1):
+                torch.save({
+                    'epoch': epoch + 1,
+                    'state_dict': model.state_dict(),
+                    'opt_dict': optimizer.state_dict(),
+                }, os.path.join(save_dir, 'models', saveName + '_epoch-' + str(epoch) + '.pth.tar'))
+                print("Save model at {}\n".format(
+                    os.path.join(save_dir, 'models', saveName + '_epoch-' + str(epoch) + '.pth.tar')))
+
+            if useTest and epoch % test_interval == (test_interval - 1):
+                model.eval()
+                start_time = timeit.default_timer()
+
+                running_loss = 0.0
+                running_corrects = 0.0
+
+                for inputs, labels in tqdm(test_dataloader):
+                    inputs = inputs.to(device)
+                    labels = labels.to(device)
+
+                    with torch.no_grad():
+                        outputs = model(inputs)
+                    probs = torch.nn.Softmax(dim=1)(outputs)
+                    preds = torch.max(probs, 1)[1]
+                    loss = criterion(outputs, labels)
+
+                    running_loss += loss.item() * inputs.size(0)
+                    running_corrects += torch.sum(preds == labels.data)
+
+                epoch_loss = running_loss / test_size
+                epoch_acc = running_corrects.double() / test_size
+
+                print("[test] Epoch: {}/{} Loss: {} Acc: {}".format(epoch + 1, nEpochs, epoch_loss, epoch_acc))
+                stop_time = timeit.default_timer()
+                print("Execution time: " + str(stop_time - start_time) + "\n")
 
 
 if __name__ == '__main__':
-    pass
+    train_model()
